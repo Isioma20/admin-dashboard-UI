8d927dc8524dbe93f457a1bf105c442f5ff917f3
Isioma20 <idiaruangela2000@gmail.com>
2025-12-18 13:53:44 +0100

    feat: Recent sales and Top orders cards UI

--- src/routes/dashboard/page.jsx
+++ src/routes/dashboard/page.jsx
@@ -4,6 +4,9 @@ import {
   DollarSign,
   CreditCard,
   Users,
+  Star,
+  PencilLine,
+  Trash,
 } from "lucide-react";
 import {
   AreaChart,
@@ -13,7 +16,7 @@ import {
   YAxis,
   Area,
 } from "recharts";
-import { overviewData } from "../../constants";
+import { overviewData, recentSalesData, topProducts } from "../../constants";
 import { useTheme } from "../../hooks/use-theme";
 
 const DashboardPage = () => {
@@ -91,7 +94,7 @@ const DashboardPage = () => {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-1 gao-4 md:grid-cols-2 lg:grid-cols-7">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7">
         <div className="card col-span-1 md:col-span-2 lg:col-span-4">
           <div className="card-header">
             <p className="card-title">Overview</p>
@@ -135,7 +138,102 @@ const DashboardPage = () => {
             </ResponsiveContainer>
           </div>
         </div>
-        <div className="card col-span-1 md:col-span-2 lg:col-span-3"></div>
+        <div className="card col-span-1 md:col-span-2 lg:col-span-3">
+          <div className="card-header">
+            <p className="card-title">Recent Sales</p>
+          </div>
+          <div className="card-body h-[300px] overflow-auto p-0">
+            {recentSalesData.map((sale) => (
+              <div
+                key={sale.id}
+                className="flex items-center justify-between py-2 pr-2"
+              >
+                <div className="flex items-center gap-x-4">
+                  <img
+                    src={sale.image}
+                    alt={sale.name}
+                    className="size-10 flex-shrink-0 rounded-full object-cover "
+                  />
+                  <div className="flex flex-col gap-y-2">
+                    <p className="font-medium text-slate-900 dark:text-slate-50">
+                      {sale.name}
+                    </p>
+                    <p className="text-sm text-slate-600 dark:text-slate-400">
+                      {sale.email}
+                    </p>
+                  </div>
+                </div>
+                <p className="font-medium text-slate-900 dark:text-slate-50">
+                  ${sale.total}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-header">
+          <p className="card-title">Top Orders</p>
+        </div>
+        <div className="card-body p-0">
+          <div className="relative h-[500px] w-full flex-shrink-0 overflow-auto rounded-none">
+            <table className="table">
+              <thead className="table-header">
+                <tr className="table-row">
+                  <th className="table-head">#</th>
+                  <th className="table-head">Product</th>
+                  <th className="table-head">Price</th>
+                  <th className="table-head">Status</th>
+                  <th className="table-head">Rating</th>
+                  <th className="table-head">Actions</th>
+                </tr>
+              </thead>
+              <tbody className="table-body">
+                {topProducts.map((product) => (
+                  <tr key={product.number} className="table-row">
+                    <td className="table-cell">{product.number}</td>
+                    <td className="table-cell">
+                      <div className="flex w-max gap-x-4">
+                        <img
+                          src={product.image}
+                          alt={product.name}
+                          className="size-14 rounded-lg object-cover"
+                        />
+                        <div className="flex flex-col">
+                          <p>{product.name}</p>
+                          <p className="font-normal text-slate-600 dark:text-slate-400">
+                            {product.description}
+                          </p>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="table-cell">{product.price}</td>
+                    <td className="table-cell">{product.status}</td>
+                    <td className="table-cell">
+                      <div className="flex items-center gap-x-2">
+                        <Star
+                          size={18}
+                          className="fill-yellow-600 stroke-yellow-600"
+                        />
+                        {product.rating}
+                      </div>
+                    </td>
+                    <td className="table-cell">
+                      <div className="flex items-center gap-x-4">
+                        <button className="text-blue-500 dark:text-blue-600">
+                          <PencilLine size={20} />
+                        </button>
+                        <button className="text-red-500">
+                          <Trash size={20} />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
